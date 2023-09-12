343332084f248d6b20f260cb6e9442c7caf5afbe
Laudin Molina Troconis <laudin.molinatroconis@silabs.com>
2023-09-12 14:24:28 +0200

    Configure todo-comments

--- lua/init.lua
+++ lua/init.lua
@@ -85,9 +85,17 @@ require("lazy").setup({
     'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
   },
   {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  --opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    --}
   }
 })
 
+require("todo-comments").setup()
 require('orgmode').setup_ts_grammar()
 
 -- Treesitter configuration
