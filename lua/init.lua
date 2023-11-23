b61533a29619cbc6832e5857b284eb8060139563
Laudin Molina Troconis <laudin.molinatroconis@silabs.com>
2023-11-23 16:16:55 +0100

    Update orgmode config

--- lua/init.lua
+++ lua/init.lua
@@ -104,10 +104,29 @@ require("lazy").setup({
   },
   {
     'nvim-orgmode/orgmode',
-    --  ft = {'org'},
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
     config = function()
-      require('orgmode').setup{}
-    end
+      -- Load treesitter grammar for org
+      require('orgmode').setup_ts_grammar()
+
+      -- Setup treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { 'org' },
+        },
+        ensure_installed = { 'org' },
+      })
+
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/org/**/*',
+        org_default_notes_file = '~/org/journal.org',
+      })
+    end,
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
